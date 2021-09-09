408c3f10953810007cc9e0eb74cdf3e2dc83129a
Pedro Andrade <pcontra@MacBook-Pro-de-Pedro.local>
2021-09-09 13:34:38 +0100

    working with getStaticPath and getStaticProps

---
+++ code-ninja-project/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p>©Copyright 2021 Ninja List</p>
+    </footer>
+  );
+};
+
+export default Footer;
--- code-ninja-project/pages/about.js
+++ code-ninja-project/pages/about.js
@@ -1,32 +1,28 @@
 import Head from "next/head";
-import Link from "next/link";
-import styles from "../styles/Home.module.css";
-
-export default function Home() {
+const About = () => {
   return (
     <>
       <Head>
-        <title>Ninja List | Home</title>
+        <title>Ninja List | About</title>
         <meta name="keywords" content="ninjas" />
       </Head>
       <div>
-        <h1 className={styles.title}>Homepage</h1>
-        <p className={styles.text}>
+        <h1>About</h1>
+        <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi iste
           a impedit assumenda sapiente vero similique voluptatem neque pariatur.
           Deserunt laboriosam voluptates ab obcaecati dolores quo beatae sunt id
           quaerat.
         </p>
-        <p className={styles.text}>
+        <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi iste
           a impedit assumenda sapiente vero similique voluptatem neque pariatur.
           Deserunt laboriosam voluptates ab obcaecati dolores quo beatae sunt id
           quaerat.
         </p>
-        <Link href="/ninjas">
-          <a className={styles.btn}>See Ninja Listing</a>
-        </Link>
       </div>
     </>
   );
-}
+};
+
+export default About;
